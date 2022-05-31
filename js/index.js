0eca84222b45b431876cfc0cf6474c244dd58d5c
vipin <vipin.gopinath@spotcap.com>
2022-05-31 23:48:47 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,61 @@
+(function ($){
+  $(window).scroll(function(){
+    $('nav').toggleClass('scrolled',$(this).scrollTop()>200);
+    $('.top-header').toggleClass('scrolleddown',$(this).scrollTop()>200);
+});
+
+    $.fn.counter = function() {
+      const $this = $(this),
+      numberFrom = parseInt($this.attr('data-from')),
+      numberTo = parseInt($this.attr('data-to')),
+      delta = numberTo - numberFrom,
+      deltaPositive = delta > 0 ? 1 : 0,
+      time = parseInt($this.attr('data-time')),
+      changeTime = 10;
+      
+      let currentNumber = numberFrom,
+      value = delta*changeTime/time;
+      var interval1;
+      const changeNumber = () => {
+        currentNumber += value;
+        //checks if currentNumber reached numberTo
+        (deltaPositive && currentNumber >= numberTo) || (!deltaPositive &&currentNumber<= numberTo) ? currentNumber=numberTo : currentNumber;
+        this.text(parseInt(currentNumber));
+        currentNumber == numberTo ? clearInterval(interval1) : currentNumber;  
+      }
+  
+      interval1 = setInterval(changeNumber,changeTime);
+    }
+  }(jQuery));
+  
+ 
+// Set the date we're counting down to
+var countDownDate = new Date("June 9, 2022 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("days").innerHTML = days ;
+  document.getElementById("hours").innerHTML=hours;
+  document.getElementById("minutes").innerHTML=minutes;
+  document.getElementById("seconds").innerHTML=seconds;
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
